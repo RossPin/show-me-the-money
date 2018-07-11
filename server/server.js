381356fd5f3e7c98fd26f43f6941f7c12d8f092d
catepalmer <catherinepalmer@gmail.com>
2018-07-12 10:42:44 +1200

    set up migrations, started user routes

--- server/server.js
+++ server/server.js
@@ -6,12 +6,16 @@ var passport = require('passport')
 
 var server = express()
 
+const authRoute = require('./routes/auth')
+
 server.use(cors('*'))
 
 server.use(passport.initialize())
 server.use(bodyParser.json())
 server.use(express.static(path.join(__dirname, '../public')))
 
-server.use('/api/auth', require('./routes/auth'))
+
+
+server.use('/api/auth', authRoute)
 
 module.exports = server
