ae1e83d5c419422de470f57fd01cf3fbab9a9c35
RossPin <dorpetz@hotmail.com>
2018-08-15 15:08:42 +1200

    home route goes to login or history depending on loggedin

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -9,33 +9,34 @@ import Meeting from './Meeting'
 import History from './History'
 import MeetingSummary from './MeetingSummary'
 
-const App = ({auth}) => (
-  <Router>
-    <div className="container has-text-centered">
+const App = ({auth}) => {
+  const redirect = auth.isAuthenticated ? History : Login
+  return (
+    <Router>
+      <div className="container has-text-centered">
 
-      <div className="hero is-small is-primary">
-        <div className="hero-body has-text-centered">
-          <Link to='/' className="">
-            <h1 className="title is-1">$how Me The Money</h1>
-          </Link>
-          <Nav />
+        <div className="hero is-small is-primary">
+          <div className="hero-body has-text-centered">
+            <Link to='/' className="">
+              <h1 className="title is-1">$how Me The Money</h1>
+            </Link>
+            <Nav />
+          </div>
         </div>
-      </div>
 
-      <div className=''>
-        {!auth.isAuthenticated &&
-          <Route exact path="/" component={Login} />
-        }
-        <Route path="/login" component={Login} />
-        <Route path="/register" component={Register} />
-        <Route exact path="/meeting" component={Meeting} />
-        <Route path="/history" component={History} />
-        <Route path='/meeting/:id' component={MeetingSummary} />
-      </div>
+        <div className=''>
+          <Route exact path="/" component={redirect} />          
+          <Route path="/login" component={Login} />
+          <Route path="/register" component={Register} />
+          <Route exact path="/meeting" component={Meeting} />
+          <Route path="/history" component={History} />
+          <Route path='/meeting/:id' component={MeetingSummary} />
+        </div>
 
-    </div>
-  </Router>
-)
+      </div>
+    </Router>
+  )
+}
 
 const mapStateToProps = ({auth}) => {
   return {
